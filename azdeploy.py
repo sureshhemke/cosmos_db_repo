a2e12f63084e2d978d9ac59d3b84e38adb4c0d29
Suresh Hemke <sureshhemke@gmail.com>
2026-09-17 14:57:30 +0530

    updated rg and location values in azdeploy file

--- azdeploy.py
+++ azdeploy.py
@@ -2,8 +2,8 @@
 # Change the values of these variables as needed.
 # =============================================================================
 
-rg = "<your-resource-group-name>"  # Resource Group name
-location = "<your-azure-region>"   # Azure region for the resources
+rg = "rg_lab"  # Resource Group name
+location = "East US   # Azure region for the resources
 
 # =============================================================================
 # DON'T CHANGE ANYTHING BELOW THIS LINE.
